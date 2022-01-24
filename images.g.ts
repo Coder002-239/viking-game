5e084f37393312a9b7723bedbe76374ff50e4cad
Coder002-239 <90915811+Coder002-239@users.noreply.github.com>
2022-01-24 10:41:27 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Player":return img`
+. . . . . . . . . . . . . . . . 
+. . . b . . . . . . . . b . . . 
+. . . b b f f f f f f b b . . . 
+. . . . b b f f f f f b . . . . 
+. . . . . f f 8 f f 8 . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . . . 5 5 5 5 . . . . . . 
+. . . . . 4 4 d 4 4 4 . . . f . 
+. . . . . 4 c c c 4 4 . . 4 . . 
+. . . . . d c f c d 4 4 4 . . . 
+. . . . . 4 c c c 4 4 . . . . . 
+. . . . . 4 4 d 4 4 4 . . . . . 
+. . . . . 4 4 4 4 4 4 . . . . . 
+. . . . . . b . . . b . . . . . 
+. . . . . . b . . . b . . . . . 
+. . . . . . f f . . f f . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
