6c95b939728a27504dd85479fdc205d1ba6f23e0
Coder002-239 <90915811+Coder002-239@users.noreply.github.com>
2022-01-28 12:08:58 -0500

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -11,21 +11,72 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         projectile = sprites.createProjectileFromSprite(assets.image`vikingStar`, Main, 100, 5)
     }
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava0, function (sprite, location) {
+    info.changeScoreBy(70)
+    music.powerDown.play()
+    tiles.placeOnTile(Main, tiles.getTileLocation(1, 25))
+})
+scene.onOverlapTile(SpriteKind.Enemy, sprites.dungeon.hazardLava1, function (sprite, location) {
+    if (sprite.isHittingTile(CollisionDirection.Bottom)) {
+        sprite.vy = -5 * pixelsToMeters
+    }
+})
+function initializeLvl2 () {
+    scene.setBackgroundImage(assets.image`backdrop2`)
+    tiles.setTilemap(tilemap`lvl2`)
+    tiles.placeOnTile(Main, tiles.getTileLocation(1, 25))
+    info.setLife(3)
+    createDragon1()
+    tiles.placeOnTile(Enemy1, tiles.getTileLocation(21, 15))
+    createDragon2()
+    tiles.placeOnTile(Enemy2, tiles.getTileLocation(30, 21))
+}
+function initializeLvl3 () {
+    scene.setBackgroundImage(assets.image`backdrop3`)
+    tiles.setTilemap(tilemap`lvl3`)
+    tiles.placeOnTile(Main, tiles.getTileLocation(1, 25))
+    info.setLife(3)
+    createDragon1()
+    tiles.placeOnTile(Enemy1, tiles.getTileLocation(11, 18))
+    createDragon2()
+    tiles.placeOnTile(Enemy2, tiles.getTileLocation(26, 21))
+    createDragon3()
+    tiles.placeOnTile(Enemy3, tiles.getTileLocation(39, 26))
+}
+scene.onOverlapTile(SpriteKind.Enemy, sprites.dungeon.hazardLava0, function (sprite, location) {
+    if (sprite.isHittingTile(CollisionDirection.Bottom)) {
+        sprite.vy = -5 * pixelsToMeters
+    }
+})
 function initializeLvl1 () {
-    tiles.setTilemap(tilemap`lvl1`)
     scene.setBackgroundImage(assets.image`backdrop1`)
+    tiles.setTilemap(tilemap`lvl1`)
     game.showLongText("Welcome to Viking CLash!", DialogLayout.Bottom)
     game.showLongText("Your goal is to get all chests loot! But it's not going to be easy! There will be enmies and  and traps!", DialogLayout.Bottom)
     game.showLongText("Use the arrow keys to move and jump!", DialogLayout.Bottom)
     game.showLongText("Good luck, and have fun!", DialogLayout.Bottom)
     createPlayer()
     tiles.placeOnTile(Main, tiles.getTileLocation(1, 25))
+    info.setLife(3)
     createDragon1()
     tiles.placeOnTile(Enemy1, tiles.getTileLocation(16, 19))
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`Chat1`, function (sprite, location) {
     game.showLongText("Watch out for lava! If you fall into it you will lose a life and have to respawn at the beginning of the level.", DialogLayout.Bottom)
     tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onHitWall(SpriteKind.Enemy, function (sprite, location) {
+    if (sprite.isHittingTile(CollisionDirection.Left)) {
+        sprite.vx = 50
+    }
+    if (sprite.isHittingTile(CollisionDirection.Right)) {
+        sprite.vx = -50
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
+    info.changeLifeBy(-1)
+    music.powerDown.play()
+    tiles.placeOnTile(Main, tiles.getTileLocation(1, 25))
 })
 function createDragon1 () {
     Enemy1 = sprites.create(assets.image`dragonEnemy1`, SpriteKind.Enemy)
@@ -42,6 +93,9 @@ function createDragon3 () {
     Enemy3.ay = 500
     Enemy3.vx = -50
 }
+scene.onOverlapTile(SpriteKind.Player, assets.tile`chest2`, function (sprite, location) {
+    game.showLongText("Great job! You have gotten a treasure chest.", DialogLayout.Bottom)
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`Chat3`, function (sprite, location) {
     game.showLongText("Watch out for the dragons! If it hits you, you will lose a life!", DialogLayout.Bottom)
     game.showLongText("Press the spacebar or \"a\" to throw a viking star and eliminate them!", DialogLayout.Bottom)
@@ -56,12 +110,15 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`chat2`, function (sprite, loc
     game.showLongText("Here is the special battle-star, which can be found in each level! Acquiring this will give you 70 extra points.", DialogLayout.Bottom)
     tiles.setTileAt(location, assets.tile`transparency16`)
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`chest1`, function (sprite, location) {
+    game.showLongText("Great job! You have gotten a treasure chest.", DialogLayout.Bottom)
+    initializeLvl2()
+})
 function createPlayer () {
     Main = sprites.create(assets.image`Player`, SpriteKind.Player)
     Main.ay = 500
     controller.moveSprite(Main, 80, 0)
     info.setScore(0)
-    info.setLife(3)
     scene.cameraFollowSprite(Main)
 }
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
@@ -75,8 +132,8 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
         scene.cameraShake(4, 500)
     })
 })
-let Enemy2: Sprite = null
 let Enemy3: Sprite = null
+let Enemy2: Sprite = null
 let Enemy1: Sprite = null
 let projectile: Sprite = null
 let Main: Sprite = null
